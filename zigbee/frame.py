d24e0c70a0293e12d9d9bd1871530063fde4d0a7
Xarlan <egor21@gmail.com>
2017-12-24 22:57:46 +0300

    Create frame.py

---
+++ zigbee/frame.py
@@ -0,0 +1,44 @@
+
+
+# MAC Layer
+
+FRAME_MAC_DST_ADDR_MODE = {             #                                                bits  11 10
+                            0 : 0x0,    # PAN identifier and address field are not present      0  0
+                            1 : 0x400,  #                                                       0  1
+                            2 : 0x800,  # Address field contains a 16 bit short address         1  0
+                            3 : 0xC00   # Address field contains a 64 bit extended address      1  1
+                          }
+
+FRAME_DSC_MAC_DST_ADDR_MODE = {                                                         # bits  11 10
+                                 0x000: 'PAN identifier and address field are not present',  #   0  0
+                                 0x400: 'Reserved',                                          #   0  1
+                                 0x800: 'Address field contains a 16 bit short address',     #   1  0
+                                 0xC00: 'Address field contains a 64 bit extended address'   #   1  1
+                     }
+
+FRAME_MAC_SRC_ADDR_MODE = {             #                                                   bits  15 14
+                            0 : 0x0,    # PAN identifier and address field are not present         0  0
+                            1 : 0x4000, #                                                          0  1
+                            2 : 0x8000, # Address field contains a 16 bit short address            1  0
+                            3 : 0xC000  # Address field contains a 64 bit extended address         1  1
+                          }
+
+FRAME_DSC_MAC_SRC_ADDR_MODE = {         #                                                   bits  15 14
+                                 0x0000: 'PAN identifier and address field are not present',  #    0  0
+                                 0x4000: 'Reserved',                                          #    0  1
+                                 0x8000: 'Address field contains a 16 bit short address',     #    1  0
+                                 0xC000: 'Address field contains a 64 bit extended address'   #    1  1
+                             }
+
+FRAME_DSC_MAC_CMD_ID = {
+                        0x1: 'Association request',
+                        0x2: 'Association response',
+                        0x3: 'Disassociation notification',
+                        0x4: 'Data request',
+                        0x5: 'PAN ID conflict notification',
+                        0x6: 'Orphan notification',
+                        0x7: 'Beacon request',
+                        0x8: 'Coordinator realignment',
+                        0x9: 'GTS request'
+                    }
+
